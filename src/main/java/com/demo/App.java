26471f9458294853ba3dfd95f4b24d9f4ed94ef4
LuckyDhiman819 <luckydhiman7988@gmail.com>
2021-09-08 13:10:08 +0530

    changes third time by Lucky

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello jenkins Try to un my code!!!!!!!!!!" );
+        System.out.println( "Hello jenkins Try to run my code!!!!!!!!!!" );
+        System.out.println( "and find the best solution then we will come to the best solution" );
     }
 }
